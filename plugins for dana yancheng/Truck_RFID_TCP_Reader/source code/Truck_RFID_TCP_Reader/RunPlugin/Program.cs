55cceb4d4734aa7bdc67973f7b57efdc1237ffe9
kevin0120 <jingwei1224@163.com>
2020-05-14 14:22:36 +0800

    正确的做法是  读的插件启动后  每隔一秒发送一次读取命令 直到10s之后超时或者再10之内收到Sn号 则插件返回成功信号，若超时则弹出提示对话框（工步也是成功的，SN为空）
    写的插件启动后 应该先每隔一秒发送命令询问 载码体的位置信息直到收到载码体信息确认再合理距离内则发送 sn写命令 ，或者超时10s则返回成功信息，同样的若超时则弹出对话框（工步也是成功的，SN为空）

--- plugins for dana yancheng/Truck_RFID_TCP_Reader/source code/Truck_RFID_TCP_Reader/RunPlugin/Program.cs
+++ plugins for dana yancheng/Truck_RFID_TCP_Reader/source code/Truck_RFID_TCP_Reader/RunPlugin/Program.cs
@@ -10,45 +10,47 @@ namespace RunPlugin
     {
         static void Main(string[] args)
         {
-            string Text1 = string.Empty;
-            byte[] buffer = new byte[3000];
-            buffer[0] = 1;
-            for (int i = 0; i < 200; i++)
-            {
-                Text1 += "0x" + buffer[i].ToString("X02") + " ";
-            }
+            //string Text1 = string.Empty;
+            //byte[] buffer = new byte[3000];
+            //buffer[0] = 0XE6;
+            //buffer[1] = 0X9D;
+            //buffer[2] = 0XA8;
+            //for (int i = 0; i < 200; i++)
+            //{
+            //    Text1 += "0x" + buffer[i].ToString("X02") + " ";
+            //}
+            //MessageBox.Show("收到字节：" + Text1);
+            //MessageBox.Show("收到字节：" + string.Join(",", buffer));
+            //MessageBox.Show("收到字节：" + Encoding.ASCII.GetString(buffer, 0, 3).Trim());
 
-            MessageBox.Show("收到字节：" + Text1);
-            MessageBox.Show("收到字节：" + string.Join(",", buffer));
+            //Console.WriteLine("hello world!".Skip(0).Take(12));
+            //byte[] test = Encoding.ASCII.GetBytes("hello world!");
 
+            ////test = Encoding.UTF8.GetBytes("杨12ab");
+            ////string val = Encoding.UTF8.GetString(test, 0, 7).Trim();
+            //string val = Encoding.ASCII.GetString(test, 0, 12).Trim();
+            //Console.WriteLine(val);
 
-            Console.WriteLine("hello world!".Skip(0).Take(12));
-            byte[] test = Encoding.ASCII.GetBytes("hello world!");
-            string val = Encoding.ASCII.GetString(test, 0, 12).Trim();
-            Console.WriteLine(val);
+            //byte[] a = test.Skip(2).Take(8).ToArray();
 
-            byte[] a = test.Skip(2).Take(8).ToArray();
+            //Console.WriteLine(Encoding.ASCII.GetString(a, 0, 8).Trim());
 
-            Console.WriteLine(Encoding.ASCII.GetString(a, 0, 8).Trim());
+            //Console.WriteLine(a.ToString());
 
-            Console.WriteLine(a.ToString());
+            //string Text=string.Empty;
+            //for (int i = 0; i < test.Length; i++)
+            //{
+            //    Text += "0x" + test[i].ToString("X02") + " ";
+            //}
 
-            string Text=string.Empty;
-            for (int i = 0; i < test.Length; i++)
-            {
-                Text += "0x" + test[i].ToString("X02") + " ";
-            }
-
-            Console.WriteLine(Text);
+            //Console.WriteLine(Text);
 
             //SetParameter();
             Plugin plug = new Plugin();
             plug.Reader("192.168.0.123", "3000");
+            //Console.WriteLine("hello");
             Console.ReadKey();
         }
-
-
-
 
 
     }
--- plugins for dana yancheng/Truck_RFID_TCP_Reader/source code/Truck_RFID_TCP_Reader/Truck_RFID_TCP_Reader/Truck_RFID_TCP_Reader.cs
+++ plugins for dana yancheng/Truck_RFID_TCP_Reader/source code/Truck_RFID_TCP_Reader/Truck_RFID_TCP_Reader/Truck_RFID_TCP_Reader.cs
@@ -36,7 +36,6 @@ namespace Truck_RFID_TCP_Reader
             var param = parameters as Parameters;
             //connect
             Connect(param.IP.Trim(), param.Port.Trim());
-
             // reader request
             byte[] cmdData = new byte[4];
             cmdData[0] = 0x68;
@@ -50,8 +49,11 @@ namespace Truck_RFID_TCP_Reader
             listenStart = true;
             //_thread = new Thread(ListenerServer);
             //_thread.Start();
+            if (client.Client != null)
+            {
+               ReadProc();
+            }
 
-            ReadProc();
             return new StepResult { Data = val, IsPassed = true };
         }
 
@@ -116,17 +118,22 @@ namespace Truck_RFID_TCP_Reader
         {
             lock (this) //由于send()中的if(Sending == true) return，所以这里不会产生阻塞，如果没有那句，多次启动该线程，会在此处排队
             {
-                if (client != null)
+                if (client.Client != null)
                 {
                     try
                     {
-                        Sending = true;
-                        SendStr temp = (SendStr)obj;
-                        byte[] comSendData = temp.SendData;
-                        sendStream.Write(comSendData, 0, comSendData.Length);
-                        Sending = false;
+                        while (sendStream.CanWrite) {
+                            lock (sendStream)
+                            {
+                                Sending = true;
+                                SendStr temp = (SendStr)obj;
+                                byte[] comSendData = temp.SendData;
+                                sendStream.Write(comSendData, 0, comSendData.Length);
+                                Sending = false;
+                            }
+                            Thread.Sleep(500);
+                        }
                         //_ComSend.Abort();
-
                     }
                     catch (Exception err)
                     {
@@ -162,9 +169,27 @@ namespace Truck_RFID_TCP_Reader
 
         }
 
+        public void theout(object source, System.Timers.ElapsedEventArgs e)
+        {
+            listenStart = false;
+            if (client.Connected) { 
+            MessageBox.Show("该工步使用的读rfid插件已经超时10s！！！，发射器可能不在合理范围之内，结束工步","提示");
+            }
+        }
+
+
         private void ReadProc()
         {
             //NetworkStream receiveStrem = client.GetStream();
+            System.Timers.Timer t = new System.Timers.Timer(10000);//实例化Timer类，设置间隔时间为1000毫秒；
+
+            t.Elapsed += new System.Timers.ElapsedEventHandler(theout);//到达时间的时候执行事件；
+
+            t.AutoReset = false;//设置是执行一次（false）还是一直执行(true)；
+
+            t.Enabled = true;//是否执行System.Timers.Timer.Elapsed事件；
+
+
             Thread.Sleep(30);
             while (listenStart)
             {
@@ -178,29 +203,52 @@ namespace Truck_RFID_TCP_Reader
                         lock (sendStream)
                         {
                             readSize = sendStream.Read(buffer, 0, 3000);
+                            //Console.WriteLine(readSize);
 
                         }
-                    }
 
+                        //string Text1 = string.Empty;
+                        //for (int i = 0; i < 100; i++)
+                        //{
+                        //    Text1 += "0x" + buffer[i].ToString("X02") + " ";
+                        //}
 
-                    //string Text1 = string.Empty;
-                    //for (int i = 0; i < 200; i++)
-                    //{
-                    //    Text1 += "0x" + buffer[i].ToString("X02") + " ";
-                    //}
+                        ////MessageBox.Show("收到字节：" + Text1);
+                        //Console.WriteLine("收到字节1：" + Text1);
 
-                    //MessageBox.Show("收到字节：" + Text1);
-
-                    if (buffer[0] == 0xAA && buffer[10] == 0x68)
+                    while (true) { 
+                    if (buffer[0] == 0xAA && buffer[3] == 0x68 && buffer[1] == 0x47)
                     {
 
-                        byte[] a = buffer.Skip(12).Take(64).ToArray();
+                        byte[] a = buffer.Skip(5).Take(64).ToArray();
                         val = Encoding.ASCII.GetString(a, 0, 64).Trim();
-
                         client.Close();
+                        Console.WriteLine(val);
                         return;
+                    } else if (buffer[0] == 0xAA && buffer[3] == 0x68 && buffer[1] == 0x07)
+                        {
+                            buffer = buffer.Skip(7).ToArray();
+                            //Console.WriteLine("收到字节2：" + string.Join(",", buffer));
+
+                        }
+                        else if (buffer[0] == 0xAA && buffer[3] == 0x68 && buffer[1] == 0x0A)
+                        {
+                            buffer = buffer.Skip(10).ToArray();
+                            //Console.WriteLine("收到字节2：" + string.Join(",", buffer));
+                        }
+
+                        else
+                        {
+                          
+                           //Console.WriteLine("收到字节3：" + string.Join(",", buffer));
+                            break;
+                        }
+                        //Console.WriteLine("收到字节4：" + string.Join(",", buffer))；
+                    }
+                        break;
 
                     }
+
 
                 }
                 catch (Exception err)
@@ -212,6 +260,8 @@ namespace Truck_RFID_TCP_Reader
                 }
                 //将缓存中的数据写入传输流
             }
+            client.Close();
+            return;
         }
 
         public void Reader(string ip, string port)
@@ -224,7 +274,7 @@ namespace Truck_RFID_TCP_Reader
             cmdData[0] = 0x68;
             cmdData[1] = 0x00;
             cmdData[2] = 0x00;
-            cmdData[3] = 0x03;
+            cmdData[3] = 0x0F;
             sendStr.SendData = Transmit(cmdData);
             Send(sendStr);
 
@@ -232,7 +282,19 @@ namespace Truck_RFID_TCP_Reader
             listenStart = true;
             //_thread = new Thread(ReadProc);
             //_thread.Start();
-            ReadProc();
+            if (client.Client != null)
+            {
+                ReadProc();
+            }
+
+            if (listenStart)
+            {
+                Console.WriteLine("Sucess");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
 
         }
 
